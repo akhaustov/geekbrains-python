6c83d3cf29f32b7857e98f31fae60fd8f601b49d
Aleksandr Khaustov <alex@rosslab.ru>
2021-03-23 08:06:51 +0300

    Task 1,2

--- lesson6/lesson6_task2.py
+++ lesson6/lesson6_task2.py
@@ -6,3 +6,16 @@
 #  длина * ширина * масса асфальта для покрытия одного кв метра дороги асфальтом,
 #  толщиной в 1 см * чи сло см толщины полотна. Проверить работу метода.
 # Например: 20м * 5000м * 25кг * 5см = 12500 т
+
+class Road:
+    def __init__(self, length, width, depth):
+        self._length = length
+        self._width = width
+        self._depth = depth
+
+    def count(self):
+        return self._length * self._width * 25 * self._depth
+
+
+road = Road(20, 5000, 5)
+print(f"Масса асфальта: {road.count()//1000} т")
