78837df633a1418381bc5c17fa5d16ce87fade91
Aleksandr Khaustov <alex@rosslab.ru>
2021-03-24 08:44:14 +0300

    Lesson6

--- lesson6/lesson6_task3.py
+++ lesson6/lesson6_task3.py
@@ -8,4 +8,22 @@
 # проверить значения атрибутов, вызвать методы экземпляров).
 
 class Worker:
-    def __init__(self, name, surname, position, income):
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+    def get_full_name(self):
+        return f"{self.name} {self.surname}"
+
+    def get_total_income(self):
+        return f"{sum(self._income.values())}"
+
+
+manager = Position("Иван", "Иванов", "Разработчик", 10000, 5000)
+print(manager.get_full_name())
+print(manager.position)
+print(manager.get_total_income())
--- lesson6/lesson6_task5.py
+++ lesson6/lesson6_task5.py
@@ -5,3 +5,34 @@
 # Handle (маркер). В каждом из классов реализовать переопределение метода draw.
 # Для каждого из классов методы должен выводить уникальное сообщение.
 # Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print(f"Запуск отрисовки {self.title}")
+
+
+class Pen(Stationery):
+    def draw(self):
+        print(f"Рисуем ручкой {self.title}")
+
+
+class Pencil(Stationery):
+    def draw(self):
+        print(f"Рисуем карандашом {self.title}")
+
+
+class Marker(Stationery):
+    def draw(self):
+        print(f"Рисуем маркером {self.title}")
+
+
+s = Stationery("Painting")
+s.draw()
+pen = Pen("Bic")
+pen.draw()
+pencil = Pencil("123")
+pencil.draw()
+marker = Marker("Prime")
+marker.draw()
